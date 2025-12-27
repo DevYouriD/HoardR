8b062e2f493c13619db8d86607db04991d39bcdf
DevYouriD <youri.dera@gmail.com>
2025-12-28 00:16:04 +0100

    Downgrade java to match available build versions

--- src/main/java/com/devyourid/hoardr/HoardrApplication.java
+++ src/main/java/com/devyourid/hoardr/HoardrApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HoardrApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(HoardrApplication.class, args);
 	}
 
