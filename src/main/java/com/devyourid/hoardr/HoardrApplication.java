7c4baed9ffc6c28fd19ac6f9841b479603015f7a
DevYouriD <youri.dera@gmail.com>
2025-12-28 13:54:52 +0100

    Minor fix

--- src/main/java/com/devyourid/hoardr/HoardrApplication.java
+++ src/main/java/com/devyourid/hoardr/HoardrApplication.java
@@ -1,9 +1,12 @@
 package com.devyourid.hoardr;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class HoardrApplication {
 
 	public static void main(String[] args) {
